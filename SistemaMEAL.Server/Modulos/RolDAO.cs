5b758662881be43c0e2ee44f64921574cd6293da
Enzo008 <enzoaguirre629@gmail.com>
2024-02-04 13:21:25 -0500

    Cambios

--- SistemaMEAL.Server/Modulos/RolDAO.cs
+++ SistemaMEAL.Server/Modulos/RolDAO.cs
@@ -2,6 +2,8 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using SistemaMEAL.Server.Models;
 using SistemaMEAL.Server.Modulos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace SistemaMEAL.Modulos
 {
@@ -9,34 +11,55 @@ namespace SistemaMEAL.Modulos
     {
         private conexionDAO cn = new conexionDAO();
 
-        public IEnumerable<Rol> Listado()
+        public IEnumerable<Rol> Listado(string? rolCod = null, string? rolNom = null, string? rolPer = null)
         {
             List<Rol> temporal = new List<Rol>();
             try
             {
                 cn.getcn.Open();
 
-                SqlCommand cmd = new SqlCommand("SP_LISTAR_ROLES", cn.getcn);
+                SqlCommand cmd = new SqlCommand("SP_BUSCAR_ROL", cn.getcn);
                 cmd.CommandType = CommandType.StoredProcedure;
-                SqlDataReader rd = cmd.ExecuteReader();
-                while (rd.Read())
+
+                cmd.Parameters.AddWithValue("@P_ROLCOD", string.IsNullOrEmpty(rolCod) ? (object)DBNull.Value : rolCod);
+                cmd.Parameters.AddWithValue("@P_ROLNOM", string.IsNullOrEmpty(rolNom) ? (object)DBNull.Value : rolNom);
+                cmd.Parameters.AddWithValue("@P_ROLPER", string.IsNullOrEmpty(rolPer) ? (object)DBNull.Value : rolPer);
+                cmd.Parameters.AddWithValue("@P_LOGIPMAQ", "192.168.1.1");
+                cmd.Parameters.AddWithValue("@P_USUANO_U", "2024");
+                cmd.Parameters.AddWithValue("@P_USUCOD_U", "0001");
+                cmd.Parameters.AddWithValue("@P_USUNOM_U", "Juan");
+                cmd.Parameters.AddWithValue("@P_USUAPEPAT_U", "Perez");
+                cmd.Parameters.AddWithValue("@P_USUAPEMAT_U", "Gomez");
+
+                SqlParameter pDescripcionMensaje = new SqlParameter("@P_DESCRIPCION_MENSAJE", SqlDbType.NVarChar, -1);
+                pDescripcionMensaje.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pDescripcionMensaje);
+
+                SqlParameter pTipoMensaje = new SqlParameter("@P_TIPO_MENSAJE", SqlDbType.Char, 1);
+                pTipoMensaje.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pTipoMensaje);
+
+                StringBuilder jsonResult = new StringBuilder();
+                SqlDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("desde jsonResult:"+jsonResult);
+                if (!reader.HasRows)
                 {
-                    temporal.Add(new Rol()
-                    {
-                        RolCod = rd.GetString(0),
-                        RolNom = rd.GetString(1),
-                        UsuIng = rd.GetString(2),
-                        FecIng = rd.IsDBNull(3) ? (DateTime?)null : rd.GetDateTime(3),
-                        UsuMod = rd.GetString(4),
-                        FecMod = rd.IsDBNull(5) ? (DateTime?)null : rd.GetDateTime(5),
-                        EstReg = rd.GetString(6)[0],
-                    });
+                    jsonResult.Append("[]");
                 }
-                rd.Close();
+                else
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("desde reader:"+reader.GetValue(0).ToString());
+                        jsonResult.Append(reader.GetValue(0).ToString());
+                    }
+                }
+                Console.WriteLine("desde jsonResult final:"+jsonResult);
+                // Deserializa la cadena JSON en una lista de objetos Estado
+                temporal = JsonConvert.DeserializeObject<List<Rol>>(jsonResult.ToString());
             }
             catch (SqlException ex)
             {
-                temporal = new List<Rol>();
                 Console.WriteLine(ex.Message);
             }
             finally
