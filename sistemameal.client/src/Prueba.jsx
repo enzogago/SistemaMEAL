e5f35d7c7233afbf11420946e46fb9c5a0c9179c
Enzo008 <enzoaguirre629@gmail.com>
2024-04-23 03:04:30 -0500

    Casa

--- sistemameal.client/src/Prueba.jsx
+++ sistemameal.client/src/Prueba.jsx
@@ -1,251 +1,122 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useTable, useGroupBy, useExpanded, useRowSelect } from "react-table";
-import { fetchData } from "./components/reusable/helper";
+import React, { useEffect, useState, Fragment } from 'react'
+import { fetchData } from './components/reusable/helper'
 
-const datos = [
-    {"nombre": "Carmen", "apellido": "Gomez", "pais": "Ecuador", "meta":"100"},
-    {"nombre": "Carlos", "apellido": "Morales", "pais": "Peru", "meta":"200"},
-    {"nombre": "Gabriela", "apellido": "Gonzalez", "pais": "Colombia", "meta":"300"},
-    {"nombre": "Ana", "apellido": "Rodriguez", "pais": "Ecuador", "meta":"400"},
-    {"nombre": "Luis", "apellido": "Martinez", "pais": "Peru", "meta":"500"},
-    {"nombre": "Jose", "apellido": "Fernandez", "pais": "Colombia", "meta":"600"},
-    {"nombre": "Patricia", "apellido": "Garcia", "pais": "Ecuador", "meta":"700"},
-    {"nombre": "Miguel", "apellido": "Morales", "pais": "Peru", "meta":"800"},
-    {"nombre": "Maria", "apellido": "Martinez", "pais": "Colombia", "meta":"900"},
-    {"nombre": "Miguel", "apellido": "Garcia", "pais": "Peru", "meta":"1000"}
-];
+const Prueba = () => {
+    const [ metas, setMetas ] = useState([])
+    const [ groupedMetas, setGroupedMetas ] = useState({})
+    const [ expandedRes, setExpandedRes ] = useState([])
+    const [ expandedInd, setExpandedInd ] = useState([])
 
-const Styles = styled.div`
-    padding: 1rem;
-
-    table {
-        border-spacing: 0;
-        border: 1px solid black;
-
-        tr {
-        :last-child {
-            td {
-            border-bottom: 0;
-            }
-        }
-        }
-
-        th,
-        td {
-        margin: 0;
-        padding: 0.5rem;
-        border-bottom: 1px solid black;
-        border-right: 1px solid black;
-
-        :last-child {
-            border-right: 0;
-        }
-        }
-    }
-
-    .pagination {
-        padding: 0.5rem;
-    }
-    `;
-
-    const IndentedSpan = styled.span`
-    margin-left: ${({ depth = 0 }) => `calc(${depth || 0.25} * 2rem)`};
- `;
-
-const Table = ({ columns, data }) => {
-    
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        prepareRow,
-        rows
-    } = useTable(
-        {
-        columns,
-        data
-        },
-        useGroupBy,
-        useExpanded,
-        useRowSelect,
-        // hooks => {
-        // hooks.visibleColumns.push(columns => {
-        //     return [
-        //     {
-        //         id: "selection",
-        //         groupByBoundary: true,
-        //         Header: ({ getToggleAllRowsSelectedProps }) => (
-        //         <div>
-        //             <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
-        //         </div>
-        //         ),
-        //         Cell: ({ row }) => (
-        //         <div>
-        //             <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
-        //         </div>
-        //         )
-        //     },
-        //     ...columns
-        //     ];
-        // });
-        // }
-    );
+    useEffect(() => {
+        fetchData('Monitoreo/Filter', (data) => {
+            setMetas(data)
+            const grouped = data.reduce((grouped, meta) => {
+                const key = `res_${meta.resAno}_${meta.resCod}`
+                if (!grouped[key]) {
+                    grouped[key] = { metas: [], resNom: meta.resNom, resNum: meta.resNum, totalMetTec: 0, totalEjeTec: 0 }
+                }
+                grouped[key].metas.push(meta)
+                grouped[key].totalMetTec += Number(meta.metMetTec)
+                grouped[key].totalEjeTec += Number(meta.metEjeTec)
+                return grouped
+            }, {})
+            console.log(grouped)
+            setGroupedMetas(grouped)
+        })
+    }, [])
 
     return (
-        <table {...getTableProps()}>
-        <thead>
-            {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>
-                    <div>
-                    {column.canGroupBy ? (
-                        // Grouped columns, add a toggle
-                        <span {...column.getGroupByToggleProps()}>
-                        {column.isGrouped ? "🛑 " : "👊 "}
-                        {column.render("Header")}
-                        </span>
-                    ) : (
-                        column.render("Header")
-                    )}
-                    </div>
-                </th>
-                ))}
-            </tr>
-            ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-            prepareRow(row);
-            return (
-                <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                    const aggregatedCellInGroupedRow =
-                    row.isGrouped &&
-                    cell.isAggregated &&
-                    !cell.column.groupByBoundary;
-                    return aggregatedCellInGroupedRow ? null : (
-                    <td {...cell.getCellProps()} colSpan={cell.isGrouped ? 6 : 1}>
-                        {cell.isGrouped ? (
-                        <>
-                            <span {...row.getToggleRowExpandedProps()}>
-                            {row.isExpanded ? "👇" : "👉"}
-                            </span>{" "}
-                            {cell.render("Cell")} ({row.subRows.length})
-                        </>
-                        ) : cell.isPlaceholder ? null : (
-                        cell.render("Cell", { editable: true })
-                        )}
-                    </td>
-                    );
-                })}
-                </tr>
-            );
-            })}
-        </tbody>
-        </table>
-    );
-    }
-
-    const IndeterminateCheckbox = React.forwardRef(
-        ({ indeterminate, ...rest }, ref) => {
-            const defaultRef = React.useRef();
-            const resolvedRef = ref || defaultRef;
-
-            React.useEffect(() => {
-            resolvedRef.current.indeterminate = indeterminate;
-            }, [resolvedRef, indeterminate]);
-
-            return (
-            <>
-                <input type="checkbox" ref={resolvedRef} {...rest} />
-            </>
-            );
-        }
-    );
-
-    const CellWithNestedRows = ({
-        value,
-        row: { canExpand, isExpanded, getToggleRowExpandedProps, depth }
-        }) => {
-            console.log(value)
-            const shortText = value && (value.length > 30 ? value.substring(0, 30) + '...' : value);
-            return (
-                <>
-                {canExpand ? (
-                    <span {...getToggleRowExpandedProps()}>{isExpanded ? "➖" : "➕"}</span>
-                ) : null}
-                <IndentedSpan depth={depth}>{shortText}</IndentedSpan>
-                </>
-            );
-    };
-
-    const Prueba = () => {
-        const [metas, setMetas] = useState([])
-        useEffect(() => {
-            fetchData('Monitoreo/Filter', setMetas)
-        }, []);
-
-        const columns = React.useMemo(
-        () => [
-            {
-                Header: "Indicador",
-                accessor: "indActResCod",
-                canGroupBy: true,
-                Cell: CellWithNestedRows,
-            },
-            {
-                Header: "Estado",
-                accessor: "estNom",
-                canGroupBy: false
-            },  
-            {
-                Header: "Nombre",
-                accessor: "nombre",
-                canGroupBy: false,
-            },
-            {
-                Header: "Meta Programatica",
-                accessor: "metMetTec",
-                canGroupBy: false,
-                Cell: ({ row }) => {
-                    console.log(row)
-                  // Si la fila puede expandirse, entonces es una fila de grupo y calculamos la suma de 'meta'
-                  // De lo contrario, es una fila individual y mostramos el valor individual
-                  if (row.canExpand) {
-                    const total = row.original.subRows.reduce((sum, record) => sum + parseFloat(record.metMetTec), 0);
-                    return `${total} (total)`;
-                  } else {
-                    return row.values.metMetTec;
-                  }
-                },
-              },
-        ],[]
-    );
-
-    // Agrupamos los datos por 'indActResAno' y 'indActResCod'
-    const groupedData = metas.reduce((allGroups, item) => {
-        const key = item.indActResAno + '-' + item.indActResCod;
-        if (!allGroups[key]) {
-            allGroups[key] = [];
-        }
-        allGroups[key].push(item);
-        return allGroups;
-    }, {});
-
-    // Convertimos los datos agrupados en un formato que react-table puede entender
-    const expandedData = Object.keys(groupedData).map(key => ({
-        indActResCod: key,
-        subRows: groupedData[key],
-    }));
-
-
-    return (
-        <div className="PowerMas_TableStatus">
-            <Table columns={columns} data={expandedData} />
+        <div className=''>
+            <table className='PowerMas_TableStatus'>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th>Estado</th>
+                        <th>Periodo</th>
+                        <th>Ubicacion</th>
+                        <th>Implementador</th>
+                        <th>Meta Programatica</th>
+                        <th>Ejeucion Programatica</th>
+                        <th>% Avance Tecnico</th>
+                        <th>Meta Presupuesto</th>
+                        <th>Ejeucion Presupuesto</th>
+                        <th>% Avance Presupuesto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(groupedMetas).map(([key, { metas, resNom, resNum, totalMetTec, totalEjeTec }]) => (
+                        <Fragment key={key}>
+                            <tr>
+                                <td>
+                                    <div 
+                                        className={`pointer bold round p_25 PowerMas_MenuIcon ${expandedRes.includes(key) ? 'PowerMas_MenuIcon--rotated' : ''}`} 
+                                        onClick={() => {
+                                            if (expandedRes.includes(key)) {
+                                                setExpandedRes(expandedRes.filter(indicator => indicator !== key));
+                                            } else {
+                                                setExpandedRes([...expandedRes, key]);
+                                            }
+                                        }}
+                                    > &gt; </div>
+                                </td>
+                                <td colSpan={5}>{resNum + ' - ' + resNom}</td>
+                                <td>{totalMetTec}</td>
+                                <td>{totalEjeTec}</td>
+                            </tr>
+                            {expandedRes.includes(key) && Object.entries(metas.reduce((grouped, meta) => {
+                                const key = `ind_${meta.indActResAno}_${meta.indActResCod}`
+                                if (!grouped[key]) {
+                                    grouped[key] = { metas: [], indActResNom: meta.indActResNom, indActResNum: meta.indActResNum, totalMetTec: 0 , totalEjeTec: 0 }
+                                }
+                                grouped[key].metas.push(meta)
+                                grouped[key].totalMetTec += Number(meta.metMetTec)
+                                grouped[key].totalEjeTec += Number(meta.metEjeTec)
+                                return grouped
+                            }, {})).map(([key, { metas: subMetas, indActResNom, indActResNum, totalMetTec, totalEjeTec }]) => (
+                                <Fragment key={key}>
+                                    <tr>
+                                        <td></td>
+                                        <td>
+                                            <div 
+                                                className={`pointer bold round p_25 PowerMas_MenuIcon ${expandedInd.includes(key) ? 'PowerMas_MenuIcon--rotated' : ''}`} 
+                                                onClick={() => {
+                                                    if (expandedInd.includes(key)) {
+                                                        setExpandedInd(expandedInd.filter(indicator => indicator !== key));
+                                                    } else {
+                                                        setExpandedInd([...expandedInd, key]);
+                                                    }
+                                                }}
+                                            > &gt; </div>
+                                        </td>
+                                        <td colSpan={4}>{indActResNum + ' - ' + indActResNom}</td>
+                                        <td>{totalMetTec}</td>
+                                        <td>{totalEjeTec}</td>
+                                    </tr>
+                                    {expandedInd.includes(key) && subMetas.map((meta, index) => (
+                                        <tr key={index} style={{visibility: expandedInd.includes(key) ? 'visible' : 'collapse'}}>
+                                            <td></td>
+                                            <td></td>
+                                            <td>{meta.estNom}</td>
+                                            <td>{meta.metMesPlaTec + ' - ' + meta.metAnoPlaTec}</td>
+                                            <td>{meta.ubiNom}</td>
+                                            <td>{meta.impNom}</td>
+                                            <td>{meta.metMetTec}</td>
+                                            <td>{meta.metEjeTec}</td>
+                                            <td>{meta.metPorAvaTec}</td>
+                                            <td>{meta.metMetPre}</td>
+                                            <td>{meta.metEjePre}</td>
+                                            <td>{meta.metPorAvaPre}</td>
+                                        </tr>
+                                    ))}
+                                </Fragment>
+                            ))}
+                        </Fragment>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    );
+    )
 }
 
-export default Prueba;
+export default Prueba
