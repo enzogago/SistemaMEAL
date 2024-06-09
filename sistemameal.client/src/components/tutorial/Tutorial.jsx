5097368bcd9d644f0bbf64aa2167c12d74629b61
Enzo008 <enzoaguirre629@gmail.com>
2024-06-09 12:34:18 -0500

    Tutorial Add

--- sistemameal.client/src/components/tutorial/Tutorial.jsx
+++ sistemameal.client/src/components/tutorial/Tutorial.jsx
@@ -6,47 +6,73 @@ const Tutorial = () => {
             <h1 className="Medium-f1_75 Small-f1_5 center"> Listado de Tutoriales</h1>
             <div className='PowerMas_Video_Tutoriales Phone_12 flex Medium-flex-row Small-flex-column gap-1'>
                 <div className='PowerMas_Home_Card Medium_4 Phone_12'>
-                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
+                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo iniciar sesión?</h3>
                     <div className='p_25'>
                         <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
-                            <iframe src="https://iframe.mediadelivery.net/embed/223841/c2747e47-b857-4c2c-ab7a-7b85ceeaad0b?autoplay=false" 
+                            <iframe src="https://iframe.mediadelivery.net/embed/223841/bd3069d8-c0b6-4aca-ac39-fcb09e2b83df?autoplay=false" 
                                     style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
                                     allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
                                     allowFullScreen={true}>
                             </iframe>
                         </div>
                     </div>
-                    <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
+                    <p className='Small-f_75 Large-f1'> En este video aprenderás a como iniciar sesión en el Sistema Web MEAL.</p>
                 </div>
                 <div className='PowerMas_Home_Card Medium_4 Phone_12'>
                     <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
                     <div className='p_25'>
                         <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
-                            <iframe src="https://iframe.mediadelivery.net/embed/223841/c2747e47-b857-4c2c-ab7a-7b85ceeaad0b?autoplay=false" 
+                            <iframe src="https://iframe.mediadelivery.net/embed/223841/ce7568de-8ec1-447b-bd00-687bddd78d6f?autoplay=false" 
                                     style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
                                     allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
                                     allowFullScreen={true}>
                             </iframe>
                         </div>
                     </div>
-                    <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
+                    <p className='Small-f_75 Large-f1'> En este video aprenderás a como cambiar tu contraseña.</p>
                 </div>
                 <div className='PowerMas_Home_Card Medium_4 Phone_12'>
-                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
+                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar los datos de mi perfil?</h3>
                     <div className='p_25'>
                         <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
-                            <iframe src="https://iframe.mediadelivery.net/embed/223841/c2747e47-b857-4c2c-ab7a-7b85ceeaad0b?autoplay=false" 
+                            <iframe src="https://iframe.mediadelivery.net/embed/223841/24dc7490-f4a3-4e44-afcc-f09ce34483e6?autoplay=false" 
                                     style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
                                     allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
                                     allowFullScreen={true}>
                             </iframe>
                         </div>
                     </div>
-                    <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
+                    <p className='Small-f_75 Large-f1'> En este video aprenderás a como cambiar los datos de tu perfil.</p>
                 </div>
             </div>
-            <div className='PowerMas_Video_Tutoriales Phone_12 flex Medium-flex-row Small-flex-column gap-1'>
+            <div className='PowerMas_Video_Tutoriales Phone_12 flex Medium-flex-row Small-flex-column gap-1 jc-center'>
                 <div className='PowerMas_Home_Card Medium_4 Phone_12'>
+                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar la foto de mi perfil?</h3>
+                    <div className='p_25'>
+                        <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
+                            <iframe src="https://iframe.mediadelivery.net/embed/223841/a8ca6822-ac18-4d18-ab5c-1165955ade66?autoplay=false" 
+                                    style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
+                                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
+                                    allowFullScreen={true}>
+                            </iframe>
+                        </div>
+                    </div>
+                    <p className='Small-f_75 Large-f1'> En este video aprenderás a como actualizar la foto de tu perfil.</p>
+                </div>
+                <div className='PowerMas_Home_Card Medium_4 Phone_12'>
+                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo registrar unidades?</h3>
+                    <div className='p_25'>
+                        <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
+                            <iframe src="https://iframe.mediadelivery.net/embed/223841/84ef9515-62a5-45e4-aaf1-5c05e943067c?autoplay=false" 
+                                    style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
+                                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
+                                    allowFullScreen={true}>
+                            </iframe>
+                        </div>
+                    </div>
+                    <p className='Small-f_75 Large-f1'> En este video aprendrás a como registrar, modificar, eliminar y buscar unidades.</p>
+                </div>
+                {/* <div className='PowerMas_Home_Card Medium_4 Phone_12'>
                     <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
                     <div className='p_25'>
                         <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
@@ -58,35 +84,9 @@ const Tutorial = () => {
                         </div>
                     </div>
                     <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
-                </div>
-                <div className='PowerMas_Home_Card Medium_4 Phone_12'>
-                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
-                    <div className='p_25'>
-                        <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
-                            <iframe src="https://iframe.mediadelivery.net/embed/223841/c2747e47-b857-4c2c-ab7a-7b85ceeaad0b?autoplay=false" 
-                                    style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
-                                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
-                                    allowFullScreen={true}>
-                            </iframe>
-                        </div>
-                    </div>
-                    <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
-                </div>
-                <div className='PowerMas_Home_Card Medium_4 Phone_12'>
-                    <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
-                    <div className='p_25'>
-                        <div style={{position: 'relative', height: '100%', paddingTop: '53.90625%'}}>
-                            <iframe src="https://iframe.mediadelivery.net/embed/223841/c2747e47-b857-4c2c-ab7a-7b85ceeaad0b?autoplay=false" 
-                                    style={{border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} 
-                                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" 
-                                    allowFullScreen={true}>
-                            </iframe>
-                        </div>
-                    </div>
-                    <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
-                </div>
+                </div> */}
             </div>
-            <div className='PowerMas_Video_Tutoriales Phone_12 flex Medium-flex-row Small-flex-column gap-1'>
+            {/* <div className='PowerMas_Video_Tutoriales Phone_12 flex Medium-flex-row Small-flex-column gap-1'>
                 <div className='PowerMas_Home_Card Medium_4 Phone_12'>
                     <h3 className="PowerMas_Tittle_Map Medium-f1 Small-f1 center"> ¿Cómo cambiar mi contraseña?</h3>
                     <div className='p_25'>
@@ -126,7 +126,7 @@ const Tutorial = () => {
                     </div>
                     <p className='Small-f_75 Large-f1'> Texto descriptivo para la card lorem ipsum alrore asdlkal asldk asld alsd asdlald as askda</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
