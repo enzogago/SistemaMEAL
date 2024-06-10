8f6a7ff59f1371c5993bd0a443d1f8054a9c190f
Enzo008 <enzoaguirre629@gmail.com>
2024-06-10 08:44:30 -0500

    menu user

--- sistemameal.client/src/components/user/MenuUser.jsx
+++ sistemameal.client/src/components/user/MenuUser.jsx
@@ -1,206 +1,102 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Notiflix from "notiflix";
-import Bar from "./Bar";
-import CryptoJS from 'crypto-js';
-import { Tree } from 'antd';
-import UserInfo from "./UserInfo";
-import { fetchDataBlock, fetchDataReturn } from "../reusable/fetchs";
-import AccessIcon from "../../icons/AccessIcon";
-import useModal from "../../hooks/useModal";
-import ModalPermission from "./ModalPermission";
+import { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import { fetchDataReturn } from '../reusable/fetchs';
+import Notiflix from 'notiflix';
 
-const generarClavesDeAcceso = (menuData) => {
-    const construirClaves = (menus, menAnoPad, menCodPad, parentKey = '') => {
-        return menus
-            .filter(menu => menu.menAnoPad === menAnoPad && menu.menCodPad === menCodPad)
-            .reduce((keys, menu) => {
-            const currentKey = parentKey ? `${parentKey}-${menu.menAno}-${menu.menCod}` : `${menu.menAno}-${menu.menCod}`;
-            keys.push(currentKey);
-            const clavesDeSubmenus = construirClaves(menus, menu.menAno, menu.menCod, currentKey);
-            return keys.concat(clavesDeSubmenus);
-        }, []);
-    };
-  
-    return construirClaves(menuData, null, null);
-};
-  
-const MenuUser = () => {
-    const navigate = useNavigate();
+const ModalPermission = ({modalVisible, estadoEditado, closeModal, user}) => {
+    const [ permisos, setPermisos ] = useState([])
+    const [ permisosActuales, setPermisosActuales ] = useState(new Set());
+    const [ permisosIniciales, setPermisosIniciales ] = useState(new Set());
+    const [selectAll, setSelectAll] = useState(false);
 
-    const { id: safeCiphertext } = useParams();
-    const ciphertext = atob(safeCiphertext);
-    // Desencripta el ID
-    const bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
-    const id = bytes.toString(CryptoJS.enc.Utf8);
-    const usuAno = id.slice(0, 4);
-    const usuCod = id.slice(4);
-    
-    const [expandedKeys, setExpandedKeys] = useState([]);
-    const [checkedKeys, setCheckedKeys] = useState([]);
-    const [autoExpandParent, setAutoExpandParent] = useState(true);
-
-    const [ menus, setMenus ] = useState([]);
-
-    const [ user, setUser ] = useState(null);
-
-    const [initialCheckedKeys, setInitialCheckedKeys] = useState([]);
-
-    const { modalVisible, estadoEditado, openModal, closeModal } = useModal();
-
-    const transformMenuData = (menuData) => {
-        // Función para construir la estructura jerárquica
-        const buildTree = (menus, menAnoPad, menCodPad, parentKey = '') => {
-            return menus
-                .filter(menu => menu.menAnoPad === menAnoPad && menu.menCodPad === menCodPad)
-                .map(menu => {
-                const currentKey = parentKey ? `${parentKey}-${menu.menAno}-${menu.menCod}` : `${menu.menAno}-${menu.menCod}`;
-                return {
-                    title:
-                    <div className="flex ai-center gap-1 PowerMas_IconsTable">
-                        <span>
-                            {menu.menNom}
-                        </span>
-                        {
-                            (menu.menRef !== '#' && menu.menRef !== '') &&
-                            <span
-                                data-tooltip-id="edit-tooltip" 
-                                data-tooltip-content="Permisos" 
-                                className='flex f1_25 p_25' 
-                                onClick={() => openModal(menu)}
-                            >
-                                <AccessIcon />
-                            </span>
-                        }
-                    </div>,
-                    key: currentKey,
-                    children: buildTree(menus, menu.menAno, menu.menCod, currentKey),
-                };
-            });
-        };
-      
-        // Iniciamos la construcción de la estructura desde el nivel más alto (menAnoPad y menCodPad nulos)
-        return buildTree(menuData, null, null);
-    };
-    
-
-    // EFECTO AL CARGAR COMPONENTE GET - LISTAR METAS
-    useEffect(() => {
-        fetchDataBlock(`Usuario/${usuAno}/${usuCod}`, setUser, '.user-block');
-
-        // Inicia el bloqueo de Notiflix
-        if (document.querySelector('.access-block')) {
-            Notiflix.Block.pulse('.access-block', {
-                svgSize: '100px',
-                svgColor: '#F87C56',
-            });
-        }
-
-        Promise.all([
-            fetchDataReturn(`Menu`),
-            fetchDataReturn(`Menu/${usuAno}/${usuCod}`),
-        ]).then(([menuData, menuAccessData]) => {
-            const treeData = transformMenuData(menuData)
-            setMenus(treeData)
-            
-            const userAccessKeys = generarClavesDeAcceso(menuAccessData);
-            setCheckedKeys(userAccessKeys);
-            setInitialCheckedKeys(userAccessKeys);
-        }).catch(error => {
-            // Maneja los errores
-            console.error('Error:', error);
-            Notiflix.Notify.failure('Ha ocurrido un error al cargar los datos.');
-        }).finally(() => {
-            // Quita el bloqueo de Notiflix una vez que todas las peticiones han terminado
-            Notiflix.Block.remove('.access-block');
-        });
-    
-    }, []);
-    
-    const onExpand = (expandedKeysValue) => {
-        setExpandedKeys(expandedKeysValue);
-        setAutoExpandParent(false);
-    };
-
-    const onCheck = (checkedKeysValue, e) => {
-        let newCheckedKeys = [...checkedKeysValue.checked];
-        if (e.checked) {
-            let keyParts = e.node.key.split('-');
-            // Comprueba si el nodo marcado es un nodo de primer nivel (nodo principal)
-            if (keyParts.length === 2) {
-                // Si es un nodo principal, añade todas las claves de sus descendientes
-                let nodeAndAllDescendants = getAllDescendants(e.node);
-                // Asegúrate de que no estás añadiendo claves duplicadas
-                nodeAndAllDescendants.forEach(key => {
-                    if (!newCheckedKeys.includes(key)) {
-                        newCheckedKeys.push(key);
-                    }
-                });
-            } else {
-                // Si no es un nodo principal, solo añade las claves de los nodos padres
-                for (let i = 0; i < keyParts.length; i += 2) {
-                    let parentKey = keyParts.slice(0, i+2).join('-');
-                    if (!newCheckedKeys.includes(parentKey)) {
-                        newCheckedKeys.push(parentKey);
-                    }
-                }
-            }
+    const handleSelectAllChange = (event) => {
+        setSelectAll(event.target.checked);
+        if (event.target.checked) {
+            // Si el usuario ha seleccionado "Seleccionar todos", selecciona todos los permisos
+            setPermisosActuales(new Set(permisos.map(permiso => permiso.perCod)));
         } else {
-            let nodeAndAllDescendants = getAllDescendants(e.node);
-            newCheckedKeys = newCheckedKeys.filter(key => !nodeAndAllDescendants.includes(key));
+            // Si el usuario ha deseleccionado "Seleccionar todos", deselecciona todos los permisos
+            setPermisosActuales(new Set());
         }
-        setCheckedKeys(newCheckedKeys);
     };
     
-    const getAllDescendants = (node) => {
-        let descendants = [node.key];
-        if (node.children) {
-            node.children.forEach(child => {
-                descendants = [...descendants, ...getAllDescendants(child)];
+    
+    useEffect(() => {
+        if (modalVisible && estadoEditado) {
+            // Inicia el bloqueo de Notiflix
+            Notiflix.Loading.pulse();
+
+            Promise.all([
+                fetchDataReturn(`Permiso/${estadoEditado.menRef}`),
+                fetchDataReturn(`Permiso/${user.usuAno}/${user.usuCod}`),
+            ]).then(([menuData, permissionAccessData]) => {
+                setPermisos(menuData)
+                const permisosInicialesSet = new Set(permissionAccessData.map(permiso => permiso.perCod));
+                setPermisosIniciales(permisosInicialesSet);
+                setPermisosActuales(permisosInicialesSet);
+
+                // Establece el estado inicial de "Seleccionar todos"
+                setSelectAll(permisosInicialesSet.size === menuData.length);
+            }).catch(error => {
+                // Maneja los errores
+                console.error('Error:', error);
+                Notiflix.Notify.failure('Ha ocurrido un error al cargar los datos.');
+            }).finally(() => {
+                // Quita el bloqueo de Notiflix una vez que todas las peticiones han terminado
+                Notiflix.Loading.remove();
             });
+
         }
-        return descendants;
+    }, [modalVisible, estadoEditado])
+
+    const handleCheckboxChange = (perCod) => {
+        setPermisosActuales(current => {
+            const newPermisos = new Set(current);
+            if (newPermisos.has(perCod)) {
+                newPermisos.delete(perCod);
+            } else {
+                newPermisos.add(perCod);
+            }
+    
+            // Actualiza el estado de 'selectAll'
+            setSelectAll(newPermisos.size === permisos.length);
+    
+            return newPermisos;
+        });
     };
+
+    const closeModalAndReset = () => {
+        closeModal();
+        setPermisos([]);
+        setSelectAll(false);
+    }
 
     const handleSubmit = async() => {
-        // Convertir los Sets a arreglos para poder trabajar con ellos
-        const currentKeys = Array.from(checkedKeys);
-        const initialKeys = Array.from(initialCheckedKeys);
-    
-        // Determinar las claves añadidas y eliminadas
-        const keysAñadidas = currentKeys.filter(key => !initialKeys.includes(key));
-        const keysEliminadas = initialKeys.filter(key => !currentKeys.includes(key));
-    
-        // Extraer menAno y menCod de las claves añadidas y eliminadas
-        const permisosInsertar = keysAñadidas.map(key => {
-            const parts = key.split('-');
-            return { menAno: parts[parts.length - 2], menCod: parts[parts.length - 1] };
-        });
-    
-        const permisosAEliminar = keysEliminadas.map(key => {
-            const parts = key.split('-');
-            return { menAno: parts[parts.length - 2], menCod: parts[parts.length - 1] };
-        });
+        // Obtiene los permisos disponibles para el menú actual
+    const permisosDisponibles = permisos.map(permiso => permiso.perCod);
 
-        const MenuUsuarioDto = {
-            MenuUsuarioInsertar: permisosInsertar,
-            MenuUsuarioEliminar: permisosAEliminar,
-            Usuario: {
-                usuAno,
-                usuCod
-            }
+    // Filtra los permisos a insertar como aquellos que están en permisosActuales y también en permisosDisponibles
+    const permisosInsertar = [...permisosActuales].filter(perCod => !permisosIniciales.has(perCod) && permisosDisponibles.includes(perCod));
+
+    // Filtra los permisos a eliminar como aquellos que están en permisosIniciales pero no en permisosActuales y están en permisosDisponibles
+    const permisosAEliminar = [...permisosIniciales].filter(perCod => !permisosActuales.has(perCod) && permisosDisponibles.includes(perCod));
+        
+        const PermisoUsuarioDto = {
+            PermisoUsuarioInsertar: permisosInsertar.map(perCod => ({ perCod })),
+            PermisoUsuarioEliminar: permisosAEliminar.map(perCod => ({ perCod })),
+            Usuario: user
         };
 
         try {
             Notiflix.Loading.pulse();
             const token = localStorage.getItem('token');
-            const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/Menu/usuario`, {
+            const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/Permiso/usuario`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify(MenuUsuarioDto),
+                body: JSON.stringify(PermisoUsuarioDto),
             });
             const data = await response.json();
             if (!response.ok) {
@@ -208,54 +104,98 @@ const MenuUser = () => {
                 return;
             }
             Notiflix.Notify.success(data.message)
-            navigate(`/permiso-user/${safeCiphertext}`);
         } catch (error) {
             console.error('Error:', error);
         } finally {
             Notiflix.Loading.remove();
         }
+
+        closeModalAndReset();
     };
-    
 
     return (
-        <>
-            <div className="bg-white h-100 flex flex-column">
-                <div className="PowerMas_Header_Form_Beneficiarie flex ai-center">
-                    <Bar currentStep={3} type='user' />
+        <Modal
+            isOpen={modalVisible}
+            onRequestClose={closeModalAndReset}
+            contentLabel="Permisos del menú"
+            ariaHideApp={false}
+            closeTimeoutMS={200}
+            style={{
+                content: {
+                    background: 'white',
+                    borderRadius: '5px',
+                    color: 'rgb(51, 51, 51)',
+                    display: 'inline',
+                    maxHeight: '100vh',
+                    width: '30%',
+                    outline: 'none',
+                    padding: '20px',
+                    position: 'none'
+                },
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    position: 'fixed',
+                }
+            }}
+        >
+            <div className='flex flex-column permission-block'>
+                <div className='overflow-auto relative'>
+                    <span 
+                        className="PowerMas_CloseModal" 
+                        onClick={closeModalAndReset}
+                    >
+                        ×
+                    </span>
+                    <h2 className='PowerMas_Title_Modal_Permission center f1_25'>Permisos del menú</h2>
+                    <h2 className='PowerMas_Title_Modal_Permission center f1_25'>{estadoEditado && estadoEditado.menNom}</h2>
                 </div>
-                <div className="flex flex-grow-1 overflow-auto p1_25">
-                    <div className="flex flex-grow-1 Large_12 gap-1">
-                        <div className="PowerMas_ListPermission PowerMas_Form_Card p1 Large_6 overflow-auto access-block">
-                            <Tree
-                                className="tree-node-custom"
-                                checkable
-                                onExpand={onExpand}
-                                expandedKeys={expandedKeys}
-                                autoExpandParent={autoExpandParent}
-                                onCheck={onCheck}
-                                checkedKeys={checkedKeys}
-                                treeData={menus}
-                                checkStrictly={true}
+                <hr className='PowerMas_Hr m_5' />
+                {  
+                    permisos.length ?
+                    <div className='flex flex-column ai-center gap_5'>
+                        <p className='PowerMas_Description_Modal_Permission center f_75'>
+                            Selecciona las funcionalidades(acciones) que deseas otorgar para este usuario.
+                        </p>
+                        <div className='flex p_25 gap_25'>
+                            <input
+                                id='all'
+                                type="checkbox"
+                                checked={selectAll}
+                                onChange={handleSelectAllChange}
                             />
+                            <label htmlFor='all'>Seleccionar todos</label>
                         </div>
-                        <div className="Large_6 overflow-auto user-block">
-                            <UserInfo user={user} />
+                        <div className='flex flex-column'>
+                            {estadoEditado && permisos.map((permission, index) => (
+                                <div className='flex p_25 gap_5' key={index}>
+                                    <input
+                                        id={permission.perCod}
+                                        type="checkbox"
+                                        className='m0'
+                                        value={permission.perCod} 
+                                        checked={permisosActuales.has(permission.perCod)}
+                                        onChange={() => handleCheckboxChange(permission.perCod)}
+                                    />
+                                    <label style={{textTransform: 'capitalize', textWrap: 'nowrap'}} htmlFor={permission.perCod}>
+                                        {permission.perNom.toLowerCase()}
+                                    </label>
+                                </div>
+                            ))}
                         </div>
+                        <button className='PowerMas_Buttom_Primary Large_6' onClick={handleSubmit}>Guardar</button>
                     </div>
-                </div>
-                <footer className="PowerMas_Buttoms_Form_Beneficiarie flex ai-center jc-center">
-                    <button onClick={() => navigate(`/form-user/${safeCiphertext}`)} className="Large_3 m_75 PowerMas_Buttom_Secondary">Atras</button>
-                    <button onClick={handleSubmit} className="Large_3 m_75 PowerMas_Buttom_Primary">Grabar y Siguiente</button>
-                </footer>
+                    :
+                    <p className='PowerMas_Description_Modal_Permission center f_75'>
+                        No se encontraron Acciones para este menú.
+                    </p>
+                }
             </div>
-            <ModalPermission 
-                modalVisible={modalVisible}
-                estadoEditado={estadoEditado}
-                closeModal={closeModal}
-                user={{usuAno,usuCod}}
-            />
-        </>
+        </Modal>
     )
 }
 
-export default MenuUser
+export default ModalPermission
